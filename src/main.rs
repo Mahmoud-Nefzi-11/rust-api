b12249c80d25f6d0b68e07816aba0f31ffbbd39d
Mahmoud Nefzi <mahmoud.nefzi@stockly.ai>
2025-02-12 14:33:11 +0100

    changed response

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    // Create a simple warp filter that responds with "Hello, World!"
+    // Define a REST endpoint that responds with the string "Hello world"
     let hello = warp::path!("hello")
-        .map(|| warp::reply::html("Hello world"));
+        .map(|| warp::reply::with_status("Hello world", warp::http::StatusCode::OK));
 
-    // Start the server on the specified address
     warp::serve(hello)
-        .run(([0, 0, 0, 0], 3033)) // binds to localhost:3030
+        .run(([0, 0, 0, 0], 3033))
         .await;
 }
